0807b5c4b3ed19fff75ce43f6d9a1fd72f3e1d0a
muner12 <bakhtmuner06@gmail.com>
2024-03-31 23:54:31 +0500

    new Changes

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require('express');
+
+
+
+
+
+const app=express();
+const PORT=process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    
+console.log(`server running on:http://localhost:${PORT}`)
+
+})
