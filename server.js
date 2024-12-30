c7734935eb426c25347af5951857fd126f769203
Bakht Munir <bakhtmuner06@gmail.com>
2024-12-30 11:38:06 +0500

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/',(req,res)=>{
 app.use(epxress.json());
 
 app.use(cors({
-    origin: ['http://localhost:5173','http://localhost:3000','http://127.0.0.1:5500','http://localhost:5175','http://localhost:5174'],
+    origin: ['http://localhost:5173','http://localhost:3000','http://127.0.0.1:5500','http://localhost:5175','http://localhost:5174','http://127.0.0.1:5501','http://127.0.0.1:5500'],
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }))
 app.use(cookieParser());
